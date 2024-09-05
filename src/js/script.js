afd4268c32ff8a02e5a6ad0b0c0300bed0b7620d
RyazDev <cuellarvalenciabrayan@gmail.com>
2024-09-04 21:54:47 -0500

    Portafolio Actualizado

--- src/js/script.js
+++ src/js/script.js
@@ -1,30 +1,36 @@
+//MENU
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navLinks = document.querySelectorAll(".nav-link");
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
-// Toggle menu
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
 
-// Close menu on link click
 navLinks.forEach(link => link.addEventListener("click", () => {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }));
 
-// Theme switch
 toggleSwitch.addEventListener("change", e => {
   const theme = e.target.checked ? "dark" : "light";
   document.documentElement.setAttribute("data-theme", theme);
   localStorage.setItem("theme", theme);
 });
 
-// Apply saved theme
 const savedTheme = localStorage.getItem("theme");
 if (savedTheme) {
   document.documentElement.setAttribute("data-theme", savedTheme);
   toggleSwitch.checked = savedTheme === "dark";
 }
+
+//Formulario
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevenir el envío real del formulario
+
+  document.getElementById('responseMessage').style.display = 'block';
+
+  document.getElementById('contactForm').reset();
+});
